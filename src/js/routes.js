6738556550ced2862e29117b96ae311cf29806f6
Juan Carlos Olvera Bear <119340160+BEAR-FAMELOVA@users.noreply.github.com>
2025-10-24 20:23:58 -0600

    inicio de app

---
+++ src/js/routes.js
@@ -0,0 +1,17 @@
+import { HomePage } from '../pages'; 
+
+var routes = [
+  {
+    path: '/',
+    component: HomePage, // Usas el componente importado
+  },
+  // Opcionalmente, podrías agregar otra ruta que importaste:
+  /*
+  {
+    path: '/acerca-de',
+    component: AboutPage, 
+  },
+  */
+];
+
+export default routes;
